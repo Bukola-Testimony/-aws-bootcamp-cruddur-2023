deea4fef099af0b93a83391fc097d0a6b965f3cc
Bukola Testimony <Bukolatestimony2@gmail.com>
2023-03-24 16:15:54 +0100

    Update user_activities.py

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+      # xray ---
+      segment = xray_recorder.begin_segment('user_activities')
+      
     model = {
       'errors': None,
       'data': None
@@ -14,10 +18,20 @@ class UserActivities:
       now = datetime.now()
       results = [{
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
+        'handle':  'Bukola testimony',
+        'message': 'Cloud is really fun!!',
         'created_at': (now - timedelta(days=1)).isoformat(),
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+      
+       subsegment = xray_recorder.begin_subsegment('bukola-mock-data')
+    # xray ---
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
+      
+     
     return model
