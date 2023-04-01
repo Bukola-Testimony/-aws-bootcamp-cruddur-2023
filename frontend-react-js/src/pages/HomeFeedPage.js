2acab6cf2374e8dee9f3455c03adbc040eeaa544
Bukola Testimony <Bukolatestimony2@gmail.com>
2023-04-01 13:41:53 +0000

    modified

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -40,7 +40,7 @@ export default function HomeFeedPage() {
     }
   };
 
-   const checkAuth = async () => {
+  const checkAuth = async () => {
     Auth.currentAuthenticatedUser({
       // Optional, By default is false. 
       // If set to true, this call will send a 
